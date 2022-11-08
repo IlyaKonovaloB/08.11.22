fa1bc49213de22e0f44fb4ce67f11e82aaba443a
IlyaKonovaloB <103986019+IlyaKonovaloB@users.noreply.github.com>
2022-11-08 12:53:23 +0300

    Add files via upload

---
+++ example_dashboard-main/index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const http = require("http");
+const path = require("path");
+const cors = require("cors");
+const morgan = require("morgan");
+
+const app = express();
+const server = http.createServer(app);
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+app.disable("x-powered-by");
+
+app.use((err, req, res, next) => {
+	logger.error(err.stack);
+	res.status(500).send("Вы сломали сервер!");
+});
+
+app.use((err, req, res, next) => {
+	if (error instanceof ForbiddenError) {
+		return res.status(403).send({
+			status: "forbidden",
+			message: error.message,
+		});
+	}
+});
+
+// Routes
+app.use("/dashboard", dashboard); //подключить
+
+app.use(express.static("client/build"));
+// самому поменять
+
+app.get("*", (req, res) => {
+	res.sendFile(path.resolve(__dirname, "../", "client", "build", "index.html"));
+	// самому поменять
+});
+
+//Go the SERVERs
+server.listen(port, () => {
+	console.log("\x1b[35m%s\x1b[0m", `The server is running on the port ${port}`);
+	console.log("\x1b[32m%s\x1b[0m", `http://localhost:${port}/`);
+	// console.log(`Worker ${cluster.worker.id} launched`);
+});
